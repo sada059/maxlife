b7677c763a83765fbccfce5abe79d19e9d3a9615
shwethasf <shwethad.patil@gmail.com>
2016-06-20 14:16:33 +0530

    updated head

--- includes/head.php
+++ includes/head.php
@@ -1,10 +1,6 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Max Life</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -14,7 +10,6 @@
 
     <!-- Custom CSS -->
     <link href="css/main.css" rel="stylesheet">
-
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
