59e873a573a1a54339c513d400e897e05a0dc0a8
sadashiva <sada059@gmail.com>
2016-06-20 14:12:16 +0530

    updated head

--- includes/head.php
+++ includes/head.php
@@ -1,4 +1,3 @@
-<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -23,4 +22,3 @@
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-</head>
