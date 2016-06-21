c389717d402641a76ba598da5aa34ca1a60cf925
shwethasf <shwethad.patil@gmail.com>
2016-06-21 12:31:35 +0530

    privacy update

--- privacy.php
+++ privacy.php
@@ -10,6 +10,7 @@ if ($id == '') {
 if ($id == '') {
   		$id = $_COOKIE['id'];
     }
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -17,15 +18,18 @@ if ($id == '') {
 <?php include_once("includes/head.php") ?>
 <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 <title>Why MaxLife | MaxLifeFoods</title>
+
 <?php include_once("includes/head.php") ?>
 <?php include_once("analyticstracking.php") ?>
 </head>
 <body>
+
 <?php include_once("includes/header.php") ?>
 <section>
     <div class="container">
         <div class="row">
-            <div class="calcbutton"><a href="food-storage-calculator.php?id=<?php echo $id; ?>">Family Food Storage Calculator</a></div>
+            <div class="calcbutton">
+            <a href="food-storage-calculator.php?id=<?php echo $id; ?>"><img src="images/icons/family-foodstorage-calculator.png" class="push-right"></a></div>
             <div class="heading-blue">
                 <h1>Privacy Policy</h1><br>
                 <p style="text-align: left;">MaxLifeFoods.com is committed to the privacy of our customers.  Our site uses an order form as well as a question form for customers to request information, products and services.  We collect only the information necessary to deliver the requested information, products and services.  We have security measures in place to secure our customers' sensitive information.  We do not store credit card numbers unless requested by the customer.  The customer information that we do store, which includes only the customer's name, address, e-mail, & phone number is used internally only for MaxLifeFoods and the customer only.  None of this information is sold to or shared with any third party, other than to provide the shipping address to shipping companies for the sole purpose of delivering requested MaxLifeFoods.com products to MaxLifeFoods.com customers.
@@ -38,6 +42,7 @@ if ($id == '') {
 </section>
 
 <?php include_once("includes/footer.php") ?>
+
 <?php include_once("googlefooter.php") ?>
 </body>
 </html>
