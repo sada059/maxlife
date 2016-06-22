e69eb6aefcf5ed70e432609712b7f7e16b97fe83
rahimpvlr <rahimpvlr1@gmail.com>
2016-06-21 17:16:44 -0700

    updated cc

--- contact.php
+++ contact.php
@@ -1,96 +1,102 @@
-<?php 
+<?php
+error_reporting(0);
 $id = $_GET['id'];
-
 if ($id == '') {
     session_start();
     if(isset($_SESSION['id']))
-      $id = $_SESSION['id'];
+        $id = $_SESSION['id'];
 }
 if ($id == '') {
-  		$id = $_COOKIE['id'];
-    }
-
+    $id = $_COOKIE['id'];
+}
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link rel="stylesheet" href="css/main_s.css" type="text/css" />
-<title>Contact Us | MaxLifeFoods</title>
-
     <?php include_once("includes/head.php") ?>
-<?php include_once("analyticstracking.php") ?>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" href="css/main_s.css" type="text/css" />
+    <title>Contact Us | MaxLifeFoods</title>
+    <script language="javascript">
+        function validate(){
+            var f=document.form1;
+            if(f.name.value==''){
+                alert('Your name is required');
+                f.name.focus();
+                return false;
+            }
+            f.command.value='update';
+            f.submit();
+        }
+    </script>
+
+    <script src="/js/jquery-1.3.2.min.js" content="text/javascript"></script>
+    <script type="text/javascript">
+        function cycleImages(){
+            var $active = $('#portfolio_cycler2 .active');
+            var $next = ($('#portfolio_cycler2 .active').next().length > 0) ? $('#portfolio_cycler2 .active').next() : $('#portfolio_cycler2 img:first');
+            $next.css('z-index',2);//move the next image up the pile
+            $active.fadeOut(1500,function(){//fade out the top image
+                $active.css('z-index',1).show().removeClass('active');//reset the z-index and unhide the image
+                $next.css('z-index',3).addClass('active');//make the next image the top one
+                if ($('#portfolio_cycler2 .active').next().length > 0) setTimeout('cycleImages()',7000);//check for a next image, and if one exists, call the function recursively
+            });
+        }
+        $(document).ready(function(){
+            // run every 7s
+            setTimeout('cycleImages()', 5500);
+        })
+    </script>
+    <?php include_once("analyticstracking.php") ?>
 </head>
 <body>
 <?php include_once("includes/header.php") ?>
-<div class="container heading">
-    <div class="row">
-        <div class="col-md-6"> <h2>Contact Us</h2></div>
-        <div class="col-md-6"> <a href="food-storage-calculator.php?id=<?php echo $id; ?>"><img src="images/icons/family-foodstorage-calculator.png" class="push-right"></a></div>
-    </div>
-</div>
-<form name="payment" method="post" action="send.php" onsubmit="return validate()">
-
 <section>
     <div class="container">
-        <div class="row contact-title">
-            <h2>Contact Information</h2>
-        </div>
-        <div class="row contact-content">
-            <div class="col-md-6">
-                <div class="form-group">
-                    <label>
-                        First Name
-                    </label>
-                    <input id="fname" name="fname" required class="form-control" type="text" />
-                </div>
-
-                <div class="form-group">
-                    <label>
-                        Email
-                    </label>
-                    <input id="lname" name="lname"  required class="form-control" type="text" />
-                </div>
-
-            </div>
-            <div class="col-md-6">
-                <div class="form-group">
-                    <label>
-                        Last Name
-                    </label>
-                    <input class="form-control"  required id="email" name="email" type="text" />
-                </div>
-
+        <div class="row">
+            <div class="calcbutton"><a href="food-storage-calculator.php?id=<?php echo $id; ?>">Family Food Storage Calculator</a></div>
+            <div class="heading-blue">
+                <h1>Contact Us</h1>
+                <font style="color: red; font-weight: bold;"><?php echo $msg; ?></font>
+                <form name="payment" id="payment" method="post" action="send.php" onsubmit="return validate()">
+                    <input type="hidden" name="command" />
+                    <fieldset>
+                        <legend>Contact Information</legend>
+                        <ol>
+                            <li>
+                                <label for=fname>First Name</label>
+                                <input id=fname name=fname type=text placeholder="First name" required autofocus>
+                            </li>
+                            <li>
+                                <label for=lname>Last Name</label>
+                                <input id=lname name=lname type=text placeholder="Last name" required>
+                            </li>
+                            <li>
+                                <label for=email>Email</label>
+                                <input id=email name=email type=email placeholder="example@domain.com" required>
+                            </li>
+                        </ol>
+                    </fieldset>
+                    <fieldset>
+                        <legend>Message</legend>
+                        <ol>
+                            <li>
+                                <textarea style="height: 200px; width: 700px;" name="question"></textarea>
+                            </li>
+                        </ol>
+                    </fieldset>
+                    <fieldset>
+                        <button type=submit>Send</button>
+                    </fieldset>
+                    <fieldset>
+                        <legend>MaxLifeFoods: &nbsp;PO Box 2335, Orem, UT &nbsp;84059</legend>
+                    </fieldset>
+                </form>
             </div>
         </div>
     </div>
 </section>
-<section>
-    <div class="container">
-        <div class="row contact-title">
-            <h2>Message</h2>
-        </div>
-        <div class="row contact-content">
-            <div class="col-md-12">
-                <div class="form-group">
-                    <textarea rows="10" cols="" name="question" class="form-control">
-                    </textarea>
-                </div>
-            </div>
-
-        </div>
-    </div>
-</section>
-<section>
-    <div class="container">
-        <div class="row btn-order">
-            <br/>  <input type="submit" class="btn btn-default" value="SEND"><br/><br/>
-        </div>
-    </div>
-</section>
-</form>
-
 <?php include_once("includes/footer.php") ?>
-
+<?php include_once("googlefooter.php") ?>
 </body>
 </html>
--- water.php
+++ water.php
@@ -24,99 +24,73 @@ error_reporting(0);
 <title>Water Filters | MaxLifeFoods</title>
 <?php include_once("includes/head.php") ?>
 <?php include_once("analyticstracking.php") ?>
+    <script language="javascript">
+        function addtocart(pid){
+            document.form1.productid.value=pid;
+            document.form1.command.value='add';
+            document.form1.submit();
+        }
+    </script>
 </head>
 <body>
+<?php include_once("includes/header.php") ?>
 
 <form name="form1">
 	<input type="hidden" name="productid" />
     <input type="hidden" name="command" />
 </form>
-<div id="content">
-
-<?php include_once("includes/header.php") ?>
-    <div class="container heading-blue">
-        <div class="row">
-            <div class="col-md-6" style="padding: 0"> <h2>H20 - Easy-Use Water Storage & Filtering</h2></div>
-            <div class="col-md-6"> <a href="food-storage-calculator.php?id=<?php echo $id; ?>"><img src="images/icons/family-foodstorage-calculator.png" class="push-right"></a></div>
-        </div>
+<div class="container heading-blue">
+    <div class="row">
+        <div class="col-md-6" style="padding: 0"> <h2>H20 - Easy-Use Water Storage & Filtering</h2></div>
+        <div class="col-md-6"> <img src="images/icons/family-foodstorage-calculator.png" class="push-right"></div>
     </div>
+</div>
 
- <section>
+<section>
     <div class="container">
         <div class="row">
-               <p> Water filters and storage containers are a necessary addition to your food storage.</p>
-            
-                     <p><a href="#">Click a product below to view more details.</a></p>
-                </div>
+            <p> Water filters and storage containers are a necessary addition to your food storage.</p>
+            <br>
+            <p><a href="#"> Click a product below to view more details.</a></p>
+        </div>
+        <?php
+        while($info = mysql_fetch_assoc($run)){
+        $pps = $info['product_price']/$info['servings'];
+        ?>
         <div class="row product">
             <div class="col-md-3">
-    <!--<table border="0"><tr><td><a href="food-storage.php?id=<?php echo $id; ?>"><img border="0" src="http://maxlifefoods.com/images/product/mainthumb.jpg" border="0"></a></td><td><b><a href="food-storage.php?id=<?php echo $id; ?>">CLICK HERE TO VIEW OUR MAIN LINE OF FOOD STORAGE.</a></b></td></tr></table><br><br>-->
-    <?php
-    while($info = mysql_fetch_assoc($run)){
-		$pps = $info['product_price']/$info['servings'];
-        ?>
-            
-                    <a href="details.php?id=<?php echo $id; ?>&pid=<?php echo $info['product_key']; ?>"><img border="0" src="images/product/<?php echo $info['product_image']; ?>" width="135" height="135" alt="<?php echo $info['product_name2']; ?>" /></a>
-                    </div>
+                <a href="details.php?id=<?php echo $id; ?>&pid=<?php echo $info['product_key']; ?>"> <img src="images/product/<?php echo $info['product_image']; ?>"></a>
+            </div>
             <div class="col-md-9">
                 <div class="row">
-                <div class="col-md-10">
-                    <h2 class="product-name" style="color:#fff;"><a href="details.php?id=<?php echo $id; ?>&pid=<?php echo $info['product_key']; ?>"><?php echo $info['product_name2']; ?></a> <?php if ($info['product_key'] == '963852' || $info['product_key'] == '820741'){ }else {$number = $pps; setlocale(LC_MONETARY, 'en_US'); echo "<!--- ".money_format('%(#10n', $number)."/Ea-->"; } ?><br /><p style="font-size: 12px; margin-top: -2px; text-indent: 40px;"><?php if ($info['product_key'] == '963852' || $info['product_key'] == '820741'){ echo "&nbsp;"; }else { echo $info['servings']." Servings"; } ?></p><a href="details.php?id=<?php echo $id; ?>&pid=<?php echo $info['product_key']; ?>"><p class="viewDetails" style="margin-top: -40px;"><?php $number = $info['product_price']; setlocale(LC_MONETARY, 'en_US'); $number2 = money_format('%(#10n',$number * 1.4); echo money_format('%(#10n', $number); if ($number > 100); ?></p></a></h2>
-          
-                    <?php
+                    <div class="col-md-10">
+                        <h2><?php echo $info['product_name2']; ?></h2>
+                    </div>
+                    <div class="col-md-2 product-price">
+                        <?php $number = $info['product_price']; setlocale(LC_MONETARY, 'en_US'); $number2 = money_format('%(#10n',$number * 1.4); echo money_format('%(#10n', $number); if ($number > 100); ?>
+                    </div>
+                </div>
+                <p> <?php
                     $position = 83;
-                    
-                    $message = $info['product_desc']; 
-					$post = substr($message, 0, $position); 
-					
+
+                    $message = $info['product_desc'];
+                    $post = substr($message, 0, $position);
+
                     echo $post;
                     echo "...";
-                    ?>                                       
-                            
-                </div>   
-<!--                            <input type="image" style="border:none;float:left" onclick="window.location='details.php?id=<?php echo $id; ?>&pid=<?php echo $info['product_key']; ?>'" name="details" src="/images/viewDetails.png" />
-                            <input type="image" style="border:none;float:right" onclick="window.location='addtocart.php?id=<?php echo $id; ?>&pid=<?php echo $info['product_key']?>&q=1&price=<?php echo $info['product_price']?>'" name="myclicker" src="/images/add_to_cart_white.jpg" > -->
-    <button class="btn btn-default btn-details" onclick="window.location='details.php?id=<?php echo $id; ?>&pid=<?php echo $info['product_key']; ?>'" name="details">View Details</button> &nbsp; &nbsp; &nbsp;
-                <button class="btn btn-default btn-add-cart" onclick="window.location='addtocart.php?id=<?php echo $id; ?>&pid=<?php echo $info['product_key']?>&q=1&price=<?php echo $info['product_price']?>'" name="myclicker"><img src="images/icons/cart-plus.png"> ADD TO CART</button>
+                    ?>      </p>
+                <button  class="btn btn-default btn-details" name="details" onclick="window.location='details.php?id=<?php echo $id; ?>&pid=<?php echo $info['product_key']; ?>'">View Details</button> &nbsp; &nbsp; &nbsp;
+                <button class="btn btn-default btn-add-cart"  name="myclicker" onclick="window.location='addtocart.php?id=<?php echo $id; ?>&pid=<?php echo $info['product_key']?>&q=1&price=<?php echo $info['product_price']?>'"><img src="images/icons/cart-plus.png"> ADD TO CART</button>
             </div>
             <div class="col-md-2">
 
             </div>
         </div>
-                                    
-                  
-           
-                   <!-- <div class="pricebox">  
-                        Price: <br>
-    
-            
-        <div class="price-box">
-                                                                <span class="regular-price" id="product-price-1">
-                        <span class="price">
-							<?php 
-								//$number = $info['product_price'];
-								//setlocale(LC_MONETARY, 'en_US');
-								//echo money_format('%(#10n', $number);
-							?>
-                        </span>                </span>
-                            
-            </div>
-    
-                        <div class="clear"></div>
-                    </div>-->
-                    
-                    
-                    
-                    
-                                        </div>
-                
-              
-    <?php
-	}
-    ?>
-                
+            <?php
+        }
+        ?>
     </div>
- </section>
+</section>
 <?php include_once("includes/footer.php") ?>
 <?php include_once("googlefooter.php") ?>
 </body>
